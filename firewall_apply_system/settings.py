14af465401584532aa9f199e3f9d107ad01687e1
TsungJu <e285171@gmail.com>
2022-07-04 21:14:25 +0800

    Add README.md

--- firewall_apply_system/settings.py
+++ firewall_apply_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['firewall-apply-system.herokuapp.com']
+ALLOWED_HOSTS = ['firewall-apply-system.herokuapp.com','localhost']
 
 
 # Application definition
