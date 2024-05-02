41dbd0f3bbb05b99bab66cce56336b99738bbb3a
TsungJu <e285171@gmail.com>
2024-05-02 15:53:37 +0800

    Testing : twitter api test

--- firewall_apply_app/views.py
+++ firewall_apply_app/views.py
@@ -27,14 +27,14 @@ class HomeListView(ListView):
 
 def apply(request):
 
-    client = tweepy.Client(
-        consumer_key,
-        consumer_secret,
-        access_token,
-        access_token_secret
-    )
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-    print(client.get_home_timeline().text)
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
 
     return render(
         request,
